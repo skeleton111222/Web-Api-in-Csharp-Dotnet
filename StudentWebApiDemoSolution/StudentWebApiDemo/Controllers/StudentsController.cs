3c3b83111d208c4ace565e26c1ad1c50d7f41601
skeleton111222 <prasannashakya111@gmail.com>
2026-01-14 09:14:54 +0545

    Added some code files

---
+++ StudentWebApiDemoSolution/StudentWebApiDemo/Controllers/StudentsController.cs
@@ -0,0 +1,59 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using StudentWebApiDemo.Models;
+
+//namespace StudentWebApiDemo.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class StudentsController : ControllerBase
+//    {
+//        [HttpGet]
+//        public IEnumerable<Student> Get()
+
+//        {
+//            return new List<Student>()
+//            {
+//            new Student() { Id = 1, Name = "Ram", Age = 20 },
+//            new Student() { Id = 2, Name = "Shyam", Age = 25 },
+//            new Student() { Id = 3, Name = "Hari", Age = 18 },
+//            };
+//        }
+//    }
+//}
+
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentWebApiDemo.Models;
+using System.Xml.Linq;
+
+namespace StudentWebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> _students = new List<Student>()
+        {
+            new Student { Id = 1, Name = "Ram", Age = 20 },
+            new Student { Id = 2, Name = "Shyam", Age = 25 },
+            new Student { Id = 3, Name = "Hari", Age = 18 },
+        };
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return _students;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            _students.Add(student);
+
+            return Ok(student);
+        }
+    }
+}
